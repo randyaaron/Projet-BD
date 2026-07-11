6ddfae7da7412ec12dc924ade0554678c59e7d8a
randy <randy.tchassem@enspy-uy1.cm>
2026-07-11 08:06:14 +0000

    fix pre_inscriptions migration

--- backend-app/database/migrations/2026_07_09_090944_add_id_classe_to_pre_inscriptions_table.php
+++ backend-app/database/migrations/2026_07_09_090944_add_id_classe_to_pre_inscriptions_table.php
@@ -1,18 +1,17 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     public function up(): void
     {
         Schema::table('pre_inscriptions', function (Blueprint $table) {
-            $table->unsignedInteger('id_classe')->nullable()->after('id_salle');
+            if (!Schema::hasColumn('pre_inscriptions', 'id_classe')) {
+                $table->unsignedInteger('id_classe')->nullable();
+            }
         });
     }
-
     public function down(): void
     {
         Schema::table('pre_inscriptions', function (Blueprint $table) {
